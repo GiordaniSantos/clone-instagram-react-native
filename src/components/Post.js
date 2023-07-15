26c96681f5b0a98d9672dfcbe5b982367c57eb86
Giordani da Silveira dos Santos <dani.santossilveira@hotmail.com>
2023-07-14 22:05:32 -0300

    adicionado component Author

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Image, Dimensions } from 'react-native'
 
+import Author from './Author'
+
 class Post extends Component {
     render(){
         return(
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image}/>
+                <Author email='giordani.santos.silveira@gmail.com' nickname='Giordani' />
             </View>
         )
     }
