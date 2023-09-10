d315cc735e988c942384588f5306d2de62841231
Giordani <dani.santossilveira@hotmail.com>
2023-09-10 20:17:55 -0300

    add tela de login

--- src/Navigator.js
+++ src/Navigator.js
@@ -2,10 +2,21 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Feed from './screens/Feed';
 import AddPhoto from './screens/AddPhoto';
 import Profile from './screens/Profile';
+import Login from './screens/Login';
+
+
+const Stack = createStackNavigator();
+
+const LoginOrProfileRouter = () =>
+    <Stack.Navigator initialRouteName='Perfil'>
+        <Stack.Screen name='Perfil' component={Profile} options={{headerShown: false}} />
+        <Stack.Screen name='Auth' component={Login} options={{headerShown: false}} />
+    </Stack.Navigator>
 
 const Tab = createBottomTabNavigator();
 
@@ -42,7 +53,7 @@ function MyTabs() {
       />
       <Tab.Screen
         name="Profile"
-        component={Profile}
+        component={LoginOrProfileRouter}
         options={{
           tabBarLabel: 'Profile',
           tabBarIcon: ({ color, size }) => (
@@ -61,3 +72,15 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+/*
+export default function MyStacks() {
+  return(
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName="Feed">
+              <Stack.Screen  name="Home" component={MyTabs} options={{headerShown: false}} />
+              <Stack.Screen name="Auth" component={Login} options={{headerShown: false}} />
+          </Stack.Navigator>
+      </NavigationContainer>
+  )        
+}*/
