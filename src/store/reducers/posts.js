5a98ed0531fd6d0da05c86b5f653915bbab799ff
Giordani <dani.santossilveira@hotmail.com>
2024-08-24 18:17:39 -0300

    obtendo os posts do firebase

--- src/store/reducers/posts.js
+++ src/store/reducers/posts.js
@@ -1,35 +1,15 @@
-import { ADD_POST, ADD_COMMENT } from "../actions/actionTypes";
+import { SET_POSTS, ADD_COMMENT } from "../actions/actionTypes";
 
 const initialState = {
-    posts: [{
-        id: Math.random(),
-        nickname: 'Giordani',
-        email: 'teste@gmail.com',
-        image: require('../../../assets/imgs/fence.jpg'),
-        comments: [{
-            nickname: 'John Ray Sheldon',
-            comment: 'Stunning!'
-        }, {
-            nickname: 'Ana Júlia',
-            comment: 'Foto linda, onde foi tirada?!'
-        }]
-    }, {
-        id: Math.random(),
-        nickname: 'Francisco',
-        email: 'francisco@gmail.com',
-        image: require('../../../assets/imgs/bw.jpg'),
-        comments: []
-    }]
+    posts: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:
+        case SET_POSTS:
             return {
                 ...state,
-                posts: state.posts.concat({
-                    ...action.payload
-                })
+                posts: action.payload
             }
         case ADD_COMMENT:
             return {
